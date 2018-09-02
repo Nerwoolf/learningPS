979192fa9fbed508ab766710661949f18d9104eb
nerwoolf <nerwoolf123@gmail.com>
2018-09-02 19:24:16 +0300

    azure

--- learn.ps1
+++ learn.ps1
@@ -1,2 +1,133 @@
-test
-sd
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+param(
+    [Parameter(Mandatory=$true)]
+    [String]$resourceGroup,
+
+    [Parameter(Mandatory=$true)]
+    [String]$location,
+
+    [Parameter(Mandatory=$true)]
+    [int]$VMNumber
+)
+begin{
+       # Connect to Azure
+    try {
+        Get-AzureRmSubscription
+    }
+    catch {
+        Connect-AzureRmAccount
+    }
+
+    $cred = Get-Credential -Message "Credentials for VM admin account"
+        # VMs configuration
+    
+        # Security rules
+        $secRuleVars = @{
+            Access = 'Allow'
+            Protocol = 'Tcp' 
+            Direction = 'Inbound'
+            SourceAddressPrefix = "*"
+            SourcePortRange = "*"
+            DestinationAddressPrefix = "*"
+        }
+
+
+
+}
+process{
+    New-AzureRmResourceGroup -Name $resourceGroup -Location $location 
+    # Create VMs
+    # Create Availbility set
+    $availSet = New-AzureRmAvailabilitySet -ResourceGroupName $resourceGroup -Location $location -name "AvailSet"`
+    -PlatformUpdateDomainCount 2 -PlatformFaultDomainCount 2 -Sku Aligned
+    # Create NSG Rules 
+    $rule1 = New-AzureRmNetworkSecurityRuleConfig -Name 'RDP-Allow' @secRuleVars -Priority 100 -DestinationPortRange 3389
+    $rule2 = New-AzureRmNetworkSecurityRuleConfig -Name 'HTTP-Allow' @secRuleVars -Priority 101 -DestinationPortRange 80
+
+    # Create public IP
+    $publicIP = New-AzureRmPublicIpAddress `
+    -ResourceGroupName "$resourceGroup" `
+    -Location "$location" `
+    -AllocationMethod "Dynamic" `
+    -Name "PublicIP"
+    # Create NetworkSecurity group 
+    $nsg = New-AzureRmNetworkSecurityGroup -Name 'NSG' -ResourceGroupName $resourceGroup -Location $location -SecurityRules $rule1, $rule2
+
+    # Create Subnet
+    $subnet = New-AzureRmVirtualNetworkSubnetConfig -Name 'Subnet' -AddressPrefix '192.168.0.0/24' -NetworkSecurityGroupId $nsg.id
+
+    # Create VirtualNet
+    $virtNet = New-AzureRmVirtualNetwork -Name 'VirtNet' -ResourceGroupName $resourceGroup -Location $location `
+    -AddressPrefix '192.168.0.0/16' -subnet $subnet
+
+    # FrontEnd pool
+    $FEpool = New-AzureRmLoadBalancerFrontendIpConfig -Name "FRontEndIp" -PublicIpAddress $publicIP
+    
+    # BackEnd pool
+    $BEpool = New-AzureRmLoadBalancerBackendAddressPoolConfig -Name "BackEndPool"
+    # Create probe
+    $probe = New-AzureRmLoadBalancerProbeConfig -Name "HTTP" -Port 80 -Protocol tcp -IntervalInSeconds 20 -ProbeCount 5
+    # Create rule 
+    $LBrool = New-AzureRmLoadBalancerRuleConfig -Name "LBRool" -FrontendIpConfiguration $FEpool -BackendAddressPool $BEpool `
+    -Probe $probe -Protocol tcp -FrontendPort 80 -BackendPort 80
+    # Create LoadBalancer
+    $loadBalancer = New-AzureRmLoadBalancer -Name "LB" -ResourceGroupName $resourceGroup -Location $location -LoadBalancingRule $LBrool `
+    -FrontendIpConfiguration $FEpool -BackendAddressPool $BEpool -Probe $probe 
+    # Create nat rules
+<#$natrules = @{}
+    for($i=1; $i -le $VMNumber; $i++){
+        $natrules[$i] = New-AzureRmLoadBalancerInboundNatRuleConfig -Name "RDP-VM$i" -Protocol tcp -FrontendPort "5000$i" -b
+    }#>
+     
+
+    # Create network adapters
+    $nics = @{}
+    
+    for($i=1; $i -le $VMNumber; $i++){
+      $nics[$i] = New-AzureRmNetworkInterface -ResourceGroupName $resourceGroup -Location $location `
+        -Name "MyNic$i" -LoadBalancerBackendAddressPool $BEpool -NetworkSecurityGroup $nsg `
+        -Subnet $virtNet.Subnets[0] 
+        }
+
+    # Create VM with configuring
+   
+   
+    for($i=1; $i -le $VMNumber; $i++){
+    
+        $vmConfig = New-AzureRmVMConfig -VMName "VM$i" -VMSize Standard_B2ms -AvailabilitySetId $availSet.Id | `
+        Set-AzureRmVMOperatingSystem -Windows -ComputerName "VM$i" -Credential $cred | `
+        Set-AzureRmVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer `
+        -Skus 2016-Datacenter -Version latest | Add-AzureRmVMNetworkInterface -Id $nics[$i].id
+
+        New-AzureRmVM -ResourceGroupName $resourceGroup -Location $location -VM $vmConfig 
+}
+    # Setting up IIS service
+    for($i=1; $i -le $VMNumber; $i++){
+    Set-AzureRmVMExtension -ResourceGroupName "$resourceGroup" `
+    -ExtensionName "IIS" `
+    -VMName "VM$i" `
+    -Location "$location" `
+    -Publisher Microsoft.Compute `
+    -ExtensionType CustomScriptExtension `
+    -asjob `
+    -TypeHandlerVersion 1.8 `
+    -SettingString '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
+    }
+}
+end{
+    Get-AzureRmResource -ResourceGroupName test
+}
